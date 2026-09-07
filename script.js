934c41fb07a98df9cebae5443abf52d4c5490e8f
Misa Mouse <misaeloliveira419@gmail.com>
2026-09-07 16:15:35 -0300

    nova tela campanhas

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+const telaPrincipal = document.getElementById("tela-principal");
 
+/* Tela campanhas */
 
+const telaCampanhas = document.getElementById("tela-campanhas");
+const botaoAbrirCampanhas = document.getElementById("botao-abrir-campanhas");
+
+function abrirTelaCampanhas() {
+  telaPrincipal.classList.remove("ativa");
+  telaCampanhas.classList.add("ativa");
+}
+
+botaoAbrirCampanhas.addEventListener("click", abrirTelaCampanhas);
