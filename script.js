3601348d11d0e8b1c22d5c2e217f9360db418026
Misa Mouse <misaeloliveira419@gmail.com>
2026-09-11 12:56:57 -0300

    css

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ function abrirTelaSite(id) {
 
   document.querySelectorAll(".tela-site").forEach(tela => tela.classList.remove("ativa"));
   destino.classList.add("ativa");
-  fundoHeaderTelaCampanha("tela-campanha");
+
+  fundoHeaderTelaCampanha(id);
 }
 
 window.abrirTelaSite = abrirTelaSite;
