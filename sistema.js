475b4eafee0873fa865692a23887f68de70ac2db
Misa Mouse <misaeloliveira419@gmail.com>
2026-09-12 12:57:57 -0300

    mecanica testes terminada

--- sistema.js
+++ sistema.js
@@ -4,20 +4,15 @@ document.getElementById("mecanica-testes").addEventListener("click", () => {
   abrirTelaSite("tela-mecanica-testes");
 });
 
-
-
 document.getElementById("criacao-personagem").addEventListener("click", () => {
   abrirTelaSite("tela-criacao-personagem");
 });
-
-
 
 document.getElementById("cenas-acoes").addEventListener("click", () => {
   abrirTelaSite("tela-cenas-acoes");
 });
 
-
-
 document.getElementById("niveis-habilidades").addEventListener("click", () => {
   abrirTelaSite("tela-niveis-habilidades");
 });
+
